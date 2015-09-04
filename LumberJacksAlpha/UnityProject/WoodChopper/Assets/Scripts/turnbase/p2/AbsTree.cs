69b9c9310b9f9d2650fb5e9c515333ea1f4d6128
Jacek Strupiński <jacek.strupinski@gmail.com>
2015-09-04 11:16:35 +0200

    Alpha Tests 1.1

--- LumberJacksAlpha/UnityProject/WoodChopper/Assets/Scripts/turnbase/p2/AbsTree.cs
+++ LumberJacksAlpha/UnityProject/WoodChopper/Assets/Scripts/turnbase/p2/AbsTree.cs
@@ -10,47 +10,50 @@ public abstract class AbsTree : MonoBehaviour
 	public TreeType type;
 	[SerializeField] string[] plantDialogs;
 	[SerializeField] string[] displayDialogs;
-
+	
 	public string plantLog {
 		get {
 			return plantDialogs[Random.Range(0,plantDialogs.Length)];
 		}
 	}
-
+	
 	public string displayLog {
 		get {
 			return displayDialogs[Random.Range(0,displayDialogs.Length)];
 		}
 	}
-
+	
 	public TreeActivateTime activateTime;
 	AbsBuff[] buffs;
 	public int plantCost=1;
-
+	
 	public int defaultTurnToLife = 2;
 	protected int turnToLifeCounter;
 	protected TreeState state;
-
+	
 	protected Vector3 originScale;
-
+	
+	virtual protected void Awake () {
+		originScale = graphicalModel.localScale;
+		startScale = originScale * 0.5f;
+	}
 	virtual protected void Start () {
 		buffs = GetComponents<AbsBuff>();
-		originScale = graphicalModel.localScale;
 	}
-
+	
 	float _timer;
 	Quaternion fallingQuat;
 	float dominoDelayTime;
-
-	float startScale = 0.5f;
+	
+	Vector3 startScale;
 	virtual protected void Update () {
 		switch (state ) {
 		case TreeState.InSeed:
 			break;
 		case TreeState.Growing:
 			_timer += Time.deltaTime;
-			graphicalModel.localScale = Vector3.Lerp(Vector3.one*startScale, originScale,_timer);
-			if ( graphicalModel.localScale == originScale ) { 
+			graphicalModel.localScale = Vector3.Lerp(startScale, originScale,_timer);
+			if ( graphicalModel.localScale == originScale ) {
 				state = TreeState.Grown;
 			}
 			break;
@@ -68,9 +71,9 @@ public abstract class AbsTree : MonoBehaviour
 				graphicalModel.rotation = Quaternion.identity;
 				p2Scene.Instance.treesInScene.Remove(this);
 				cell.RemoveTree();
-
+				
 				ActivateOnFall(choper);
-
+				
 				if ( dealDmg ) {
 					var c = cell.Get(fx,fz);
 					if ( c != null ) {
@@ -81,15 +84,15 @@ public abstract class AbsTree : MonoBehaviour
 			}
 			_timer += Time.deltaTime;
 			graphicalModel.rotation = Quaternion.Lerp(Quaternion.identity, fallingQuat,_timer);
-		
+			
 			break;
 		}
 	}
-
+	
 	bool dealDmg = false;
 	virtual public void OnTurnStart (int turn_nb ) {
 	}
-
+	
 	//---------------------------------- state query -----------------------------------------
 	virtual public bool IsPassable () {
 		return false;
@@ -98,26 +101,26 @@ public abstract class AbsTree : MonoBehaviour
 	virtual public bool CanBeAffectedByDomino () {
 		return true;
 	}
-
+	
 	virtual public bool PassDominoFuther () {
 		return state == TreeState.Grown | state == TreeState.Growing;
 	}
-
+	
 	//------------------------------- player messages --------------------------------------------------
 	
 	virtual public void OnBeingPlant (p2Player p , int deltaTurn ) {
 		turnToLifeCounter = defaultTurnToLife + deltaTurn;
 		p2Scene.Instance.treesInScene.Add(this);
-
-		fx = 0; 
+		
+		fx = 0;
 		fz = 0;
 		dealDmg = false;
 		state = TreeState.InSeed;
-		graphicalModel.localScale = Vector3.one*startScale;
+		graphicalModel.localScale = startScale;
 	}
-
+	
 	p2Player choper;
-
+	
 	int fx,fz;
 	virtual public void OnBeingChoped (  p2Player player, p2Cell sourceCell , int tier, int acCost=0) {
 		if ( state == TreeState.Falling | state == TreeState.WaitDomino ) {
@@ -126,14 +129,14 @@ public abstract class AbsTree : MonoBehaviour
 		fx = cell.x- sourceCell.x;
 		fz = cell.z- sourceCell.z;
 		choper = player;
-
+		
 		ActivateOnChop(player, ref fx, ref fz);
-
+		
 		if ( !(fx == 0 & fz == 0 ) ) {
 			if ( tier == 0 ) player.OnChopDone(acCost);
 			fallingQuat = Angle.Convert(fx,fz);
 			dominoDelayTime = tier * p2Scene.Instance.globalDominoDelay;
-
+			
 			if ( PassDominoFuther() ) {
 				p2Cell c;
 				if ( (c= cell.Get(fx,fz) ) != null ) {
@@ -142,24 +145,24 @@ public abstract class AbsTree : MonoBehaviour
 					} else player.OnCredit(tier);
 				} else player.OnCredit(tier);
 			} else player.OnCredit(state == TreeState.InSeed? tier-1 : tier);
-
+			
 			dealDmg = state != TreeState.InSeed;
 			state = TreeState.WaitDomino;
 		}
 	}
-
+	
 	virtual protected void ActivateOnChop (p2Player player, ref int fx, ref int fz ) {
 	}
-
+	
 	virtual protected void ActivateOnFall (p2Player player ) {
 	}
-
+	
 	virtual public bool Activate () {
 		if ( state == TreeState.InSeed ) return false;
 		return true;
 	}
-
-
+	
+	
 	//------------------------------ update per turn ----------------------------------------
 	virtual public void OnBackgroundUpdate (List<p2Player> players ) {
 		if ( turnToLifeCounter < 0 ) {
@@ -182,4 +185,3 @@ public abstract class AbsTree : MonoBehaviour
 		return !isActiveAndEnabled;
 	}
 }
-
